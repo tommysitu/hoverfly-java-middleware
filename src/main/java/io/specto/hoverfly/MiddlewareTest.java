d00fbcf90ca082726483b49bc293ad568837ac64
Tommy Situ <tommy.situ@specto.io>
2017-05-31 16:04:57 +0100

    Java middlware to modify response body

--- src/main/java/io/specto/hoverfly/MiddlewareTest.java
+++ src/main/java/io/specto/hoverfly/MiddlewareTest.java
@@ -1,20 +1,30 @@
 package io.specto.hoverfly;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import java.util.Scanner;
 
 public class MiddlewareTest {
 
+
     public static void main(String[] args)  {
         Scanner scanner = new Scanner(System.in);
+        ObjectMapper objectMapper = new ObjectMapper();
 
         if (scanner.hasNextLine()) {
             String payload = scanner.nextLine();
             try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                JsonNode pairs = objectMapper.readTree(payload);
+                if (pairs.has("response")) {
+                    ObjectNode responseNode = (ObjectNode) pairs.get("response");
+                    responseNode.put("body", "body was replaced by middleware\\n");
+                }
+                System.out.print(objectMapper.writeValueAsString(pairs));
+            } catch (Exception e) {
+                throw new IllegalStateException("Failed reading from payload: " + payload);
             }
-            System.out.print("{\"response\":{\"status\":200,\"body\":\"{\\n   \\\"time\\\": \\\"hacked\\\",\\n   \\\"milliseconds_since_epoch\\\": 1494840232933,\\n   \\\"date\\\": \\\"05-15-2017\\\"\\n}\\n\",\"encodedBody\":false,\"headers\":{\"Access-Control-Allow-Origin\":[\"*\"],\"Content-Length\":[\"100\"],\"Content-Type\":[\"application/json; charset=ISO-8859-1\"],\"Date\":[\"Mon, 15 May 2017 09:23:52 GMT\"],\"Hoverfly\":[\"Was-Here\"],\"Server\":[\"Google Frontend\"],\"X-Cloud-Trace-Context\":[\"cc24ab0d80ef1ae731f1c47eb1a3bc82\"]}},\"request\":{\"path\":\"/\",\"method\":\"GET\",\"destination\":\"time.jsontest.com\",\"scheme\":\"http\",\"query\":\"\",\"body\":\"\"}}");
         }
     }
 }
